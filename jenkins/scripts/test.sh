9b17c0efbb5c433eb9f90635487cbd81e68a8938
hisbu <lab.hisbu@gmail.com>
2020-08-11 02:18:24 +0000

    test

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -10,6 +10,7 @@ echo 'installing this dependency, it would typically be done so using this'
 echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
 set -x
+npm install
 npm install --save-dev cross-env
 set +x
 
